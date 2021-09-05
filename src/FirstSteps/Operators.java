90c1640462b082383ed9842ec43c3fefda0430a4
Harshit Kapoor <harshitk@gmail.com>
2021-09-05 15:57:24 +0530

    New Changes

---
+++ src/FirstSteps/Operators.java
@@ -0,0 +1,21 @@
+package FirstSteps;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int result = 1+2;
+        System.out.println(result);
+        result = result-1;
+        System.out.println(result);
+        int previousResult = result;
+        result = result * 10;
+        System.out.println(result);
+
+        result = result/4;
+        System.out.println(result);
+
+        int newRes = result%3;
+        System.out.println(newRes);
+    }
+}
