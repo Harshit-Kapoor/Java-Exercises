eaaad8994c61acf6e359746bd33334d59d9b720a
Harshit Kapoor <51022801+Harshit-Kapoor@users.noreply.github.com>
2022-02-24 02:02:30 +0530

    Adding 4 pattern questions
    Adding 4 pattern questions

---
+++ src/Patterns/Pattern8.java
@@ -0,0 +1,38 @@
+/*
+	 				*	
+				*		
+			*			
+		*				
+	*					
+*/
+
+package Patterns;
+import java.util.*;
+
+public class Pattern8 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+
+        int n = scn.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= n; j++) {
+
+                if (i+j == n+1) {
+                    System.out.print("*\t");
+                } else {
+                    System.out.print("\t");                    
+                }
+                
+            }  
+
+            System.out.println();              
+
+        }
+
+    }
+}
